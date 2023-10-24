8947bc33eb8aa505ed3957f4514a22c7a409d057
olgaleticialopes <leticiaolgalopes@gmail.com>
2023-10-23 22:45:12 -0300

    Aula 2310 - Componente Navbar router link

--- lifedev/src/App.jsx
+++ lifedev/src/App.jsx
@@ -2,18 +2,21 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import About from './pages/About/About'
 import Home from './pages/Home/Home'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Navbar />
         <div className="conteiner">
           <Routes>
             <Route path="/" element={<Home />}> </Route>
             <Route path="/about" element={<About />}> </Route>
-            
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </>
   )
